0b08b492c6dab73e1753cf12e5f6c754cca1010a
Michael McDowell <michael.mcdowell@hotmail.co.uk>
2020-03-08 21:46:14 +0900

    fixing issues with .gitignore not ignoring bin/ and obj/ folders

---
+++ Person oop/Person oop/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace Person_oop
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string Contact { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public Person(string aName, string aIndentity, string aContact, string aAdress, string aEmail)
+        {
+            Name = aName;
+            Id = aIndentity;
+            Contact = aContact;
+            Address = aAdress;
+            Email = aEmail;
+        }
+
+        public override string ToString()
+        {
+            return $"Name:  {Name} \n Identity: {Id} \n Contact: {Contact} \n Adress: {Contact} \n Email: {Email}";
+        }
+    }
+}
